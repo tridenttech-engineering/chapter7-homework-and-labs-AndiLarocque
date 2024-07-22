b53f60aad84879c755484b562a732b17eb03a632
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-22 11:45:00 -0400

    Update lab7-3.cpp

--- lab7-3.cpp
+++ lab7-3.cpp
@@ -8,6 +8,26 @@ using namespace std;
 
 int main()
 {	
+    {
+    int day = 1;
+    int totalTexts= 0;
+        int dailyTexts= 0;
+        double average = 0.0;
 
-    return 0;
-}   //end of main function
+    while (day < 8)
+
+    {
+        cout<<"How many text messages did you send on day "
+            << day << "? ";
+        cin>>dailyTexts;
+        totalTexts+= dailyTexts;
+        day += 1;
+
+    } //end while
+
+        average = static_cast<double>(totalTexts) / (day - 1);
+        cout<<fixed <<setprecision(0);
+        cout<<endl<<"You sent approximately "<<average <<" text messages per day."<<endl;
+
+        return 0;
+    } //end of main function
